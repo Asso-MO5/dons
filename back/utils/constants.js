916e0d8f5c1ae90b76c923658f01609227dd99f8
kazerlelutin <b@bouteiller.contact>
2026-08-01 19:32:43 +0200

    feat(db): add memberships table and discord_pseudo column
    - New migrations: add discord_pseudo to users, create memberships table
    - New service membership.service (saveMembership, getMembershipById)
    - user.service: persist discord_pseudo + add updateUser helper
    - constants: MEMBERSHIP_TYPE (NEW, RENEWAL), MEMBERSHIP_AMOUNT_EUR

--- back/utils/constants.js
+++ back/utils/constants.js
@@ -3,6 +3,7 @@ const FROM = `💾🖱️🎮 ASSOCIATION MO5 | DONS <${process.env.MAIL_ADDRESS
 const DONATION_TYPE = {
   material: "MATERIAL",
   financial: "FINANCIAL",
+  membership: "MEMBERSHIP",
 }
 
 const FINANCIAL_DONATION_SOURCE = {
@@ -11,8 +12,17 @@ const FINANCIAL_DONATION_SOURCE = {
   bank_transfer: "BANK_TRANSFER",
 }
 
+const MEMBERSHIP_TYPE = {
+  new: "NEW",
+  renewal: "RENEWAL",
+}
+
+const MEMBERSHIP_AMOUNT_EUR = 15
+
 module.exports = {
   FROM,
   DONATION_TYPE,
   FINANCIAL_DONATION_SOURCE,
+  MEMBERSHIP_TYPE,
+  MEMBERSHIP_AMOUNT_EUR,
 }
